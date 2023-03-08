a39082cdc20e76272e7a982d366719828868723c
wagerc97 <wager.clemens@gmail.com>
2023-03-08 21:07:17 +0100

    added optimizer prototype

---
+++ PyMOO/optimizeModel.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+Wrapper to optimize a given ML model.
+
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+import pymoo_helpers as helper
+from optimizer import Optimizer
+from sklearn.metrics import r2_score
+import myconfig as cfg
+
+def main():
+
+    ### invoke a new instance of Optimizer ###
+    myOptimizer = Optimizer()
+
+    ### Define Algorithm for optimization ###
+    myOptimizer.setAlgorithm('NSGA2')       # genetic algorithm
+
+    ### Load model pipeline from file ###
+    loadedModel = myOptimizer.fetchPipelineFromFile()
+    print("Loaded model:\n", loadedModel)
+
+    #testScore = helper.getTestScore(loadedModel, X_test, y_test)
+    #testScore = loadedModel.score(predData.X_test, predData.y_test)
+    pred_y = loadedModel.predict(predictionInputData.X_test)
+    testScore = r2_score(y_true=predictionInputData.y_test, y_pred=pred_y)
+    print("Model test score (R²): ", round(testScore, 3))
+
+
+if __name__ == '__main__':
+    main()
---
+++ PyMOO/optimizer.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python3
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+Run optimization algorithm over ML model to find optimal solution.
+The model approximates the original mathematical problem. 
+Thus, the optimal solution only approximates the analytical solution.
+
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+import joblib
+import myconfig
+
+import pymoo_helpers as helper
+import os
+import pandas as pd  # data handling
+import numpy as np  # maths and stuff
+import matplotlib.pyplot as plt  # plot stuff
+from pymoo.optimize import minimize  # minimize solution
+
+class Optimizer:
+    """
+    A class to optimize a given ml model and return the optimum.
+    TODO: define mode (maximum or minimum)
+    User can provide the algorithm upon initialization.
+    """
+
+    def __init__(self, algorithm_name='nsga2', seed=42):
+        self.seed = seed                        # random seed
+        self.pipeline = None                    # model pipeline
+        self.algorithm_name = algorithm_name    # nsga2 is default
+        self.algorithm = None                   # Pymoo algorithm object
+        self.setAlgorithm()
+        self.pop_size = None                    # population size in genetic algorithms
+        self.iters = 100                        # number of iterations in minimization
+        self.solution = None                    # optimized solution
+
+
+    def fetchPipelineFromFile(self, filepath=myconfig.MODEL_FILE):
+        """ Load the model from disk with provided path. """
+        print("\nLoad model from file:", filepath)
+        self.pipeline = joblib.load(filepath)
+
+
+    def setPipelineObject(self, pipeline_object):
+        """ Provide pipeline object as input parameter and save it on class. """
+        self.pipeline = pipeline_object
+
+
+    def setAlgorithm(self, param_algo=None):
+        """ Create an algorithm. Default is 'NSGA2'. """
+        if param_algo is None and self.algorithm_name is not None:
+            param_algo = self.algorithm_name
+
+        if param_algo.lower() == "nsga2" or "n":
+            from pymoo.algorithms.moo.nsga2 import NSGA2
+            # use algorithm specific default if no pop size was defined by user
+            pop = 100 if self.pop_size is None else self.pop_size
+            self.algorithm = NSGA2(pop_size=pop)
+        else:
+            raise Exception("Enter parameter { NSGA2 } ... i have not implemented any other algorithms yet")
+
+
+    def setPopulationSize(self, param_pop_size):
+        """ Set a new population size for genetic algorithms. Default depends on algorithm. """
+        self.pop_size = param_pop_size
+
+
+    def setIterations(self, param_iters):
+        """ Set a new population size for genetic algorithms. Default depends on algorithm. """
+        self.iters = param_iters
+
+
+    def solve(self, verbose=False):
+        """ Solve the problem with a chosen algorithm. """
+        # Define Result
+        # https://pymoo.org/interface/minimize.html
+        iters = self.iters
+        self.solution = minimize(
+            problem=self.pipeline.predict(),
+            algorithm=self.algorithm,
+            seed=self.seed,
+            ('n_gen', iters),   # n_gen defines the number of iterations
+            verbose=verbose     # prints out solution in each iteration
+            )
